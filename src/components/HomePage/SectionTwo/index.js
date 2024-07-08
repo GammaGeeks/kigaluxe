9a749d5b0340c201019a746e52b808977d6875de
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2024-07-08 03:11:55 +0200

    New cards

--- src/components/HomePage/SectionTwo/index.js
+++ src/components/HomePage/SectionTwo/index.js
@@ -2,15 +2,54 @@ import React from 'react'
 
 import './index.scss'
 
+const cardImage = require('../../../assets/HouseOne 1.png')
+
+// SectionTwo component
 const SectionTwo = () => {
   return (
     <div className='sectionTwo'>
       <div className='title'>
         <span>Trending Properties</span>
       </div>
-      <div className='cards-container'>
-        <div className='cards'><span>Trending Properties</span></div>
-        <div className='pagination'><span>Trending Properties</span></div>
+      <div className="card-container">
+        <div className="card">
+            <div className="card-image">
+              <img src={cardImage} alt="A beautiful house with a garden and a pool." />
+            </div>
+            <div className="card-content">
+              <h2 className="card-title">Card Title</h2>
+              <p className="card-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                tincidunt arcu vel arcu fermentum, eget accumsan tincidunt.
+              </p>
+            </div>
+          </div>
+        <div className="card">
+          <div className="card-content">
+            {/* Add your card content here */}
+            <h2>Card Title</h2>
+            <p>Card description goes here.</p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-content">
+            {/* Add your card content here */}
+            <h2>Card Title</h2>
+            <p>Card description goes here.</p>
+          </div>
+        </div>
+        <div className="card">
+        <div className="card-image">
+          <img src={cardImage} alt="Card Image" />
+        </div>
+        <div className="card-content">
+          <h2 className="card-title">Card Title</h2>
+          <p className="card-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            tincidunt arcu vel arcu fermentum, eget accumsan tincidunt.
+          </p>
+        </div>
+        </div>
       </div>
     </div>
   )
