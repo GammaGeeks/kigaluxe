284b1ac2e2006b813e9db6af936c76cdf32b8c4a
Igor J.L. <nigorjeanluc@gmail.com>
2024-07-08 03:21:34 +0200

    new

--- src/components/HomePage/SectionTwo/index.js
+++ src/components/HomePage/SectionTwo/index.js
@@ -2,55 +2,31 @@ import React from 'react'
 
 import './index.scss'
 
-const cardImage = require('../../../assets/HouseOne 1.png')
-
-// SectionTwo component
 const SectionTwo = () => {
   return (
     <div className='sectionTwo'>
       <div className='title'>
         <span>Trending Properties</span>
       </div>
-      <div className="card-container">
-        <div className="card">
-            <div className="card-image">
-              <img src={cardImage} alt="A beautiful house with a garden and a pool." />
-            </div>
-            <div className="card-content">
-              <h2 className="card-title">Card Title</h2>
-              <p className="card-description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                tincidunt arcu vel arcu fermentum, eget accumsan tincidunt.
-              </p>
-            </div>
-          </div>
-        <div className="card">
-          <div className="card-content">
-            {/* Add your card content here */}
-            <h2>Card Title</h2>
-            <p>Card description goes here.</p>
-          </div>
+      <div class="card-container">
+        <div class="card">
+          <h3>Card 1</h3>
+          <p>This is the content of card 1.</p>
         </div>
-        <div className="card">
-          <div className="card-content">
-            {/* Add your card content here */}
-            <h2>Card Title</h2>
-            <p>Card description goes here.</p>
-          </div>
+        <div class="card">
+          <h3>Card 2</h3>
+          <p>This is the content of card 2.</p>
         </div>
-        <div className="card">
-        <div className="card-image">
-          <img src={cardImage} alt="Card Image" />
+        <div class="card">
+          <h3>Card 3</h3>
+          <p>This is the content of card 3.</p>
         </div>
-        <div className="card-content">
-          <h2 className="card-title">Card Title</h2>
-          <p className="card-description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            tincidunt arcu vel arcu fermentum, eget accumsan tincidunt.
-          </p>
-        </div>
+        <div class="card">
+          <h3>Card 4</h3>
+          <p>This is the content of card 4.</p>
         </div>
       </div>
+
     </div>
   )
 }
