de2259c34d9d5aad163cdb108e737c4f9d751725
Igor J.L. <nigorjeanluc@gmail.com>
2024-07-09 18:18:15 +0200

    Responsive card

---
+++ src/components/HomePage/Section_2/HomeCard/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { Stack, Row, Col, Card, Button } from 'react-bootstrap'
+
+import './index.scss'
+
+const HomeCard = () => {
+  return (
+    <Card>
+      <Card.Img
+        src={require('../../../../assets/HouseOne 1.png')}
+        style={{
+          objectFit: 'cover',
+        }}
+      />
+      <Card.Body
+        style={{ width: '60%' }}
+      >
+        <Card.Title>House for sale in Kigali</Card.Title>
+        <Card.Text>
+          <span><FontAwesomeIcon icon={faLocationDot} /> Kigali City, Gasabo, Kimihurura</span>
+          <p>Published on 12 June | Expires on 12 July</p>
+          <p><b>Details:</b> 4 bedrooms, 2 bathrooms</p>
+          <div className="badges">
+            <span className='type'>For Sale</span>
+            <span className='price'><b>Price:</b> $200,000</span>
+          </div>
+          <Button variant="primary">View Property</Button>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default HomeCard
--- src/components/HomePage/Section_2/index.js
+++ src/components/HomePage/Section_2/index.js
@@ -4,7 +4,9 @@ import React from 'react'
 import './index.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
-import { Stack, Row, Col, Card } from 'react-bootstrap'
+import {  Row, Col } from 'react-bootstrap'
+
+import HomeCard from './HomeCard'
 
 const SectionTwo = () => {
   return (
@@ -14,106 +16,17 @@ const SectionTwo = () => {
       </div>
       <div className="row card-container">
         <Col md={6}>
-          <Card>
-            <Stack direction="horizontal" gap={3}>
-              <Col className="image">
-                <img src={require('../../../assets/HouseOne 1.png')} alt="Card Image" />
-              </Col>
-              <Col className="content">
-                <h2>House for sale in Kigali</h2>
-                <span><FontAwesomeIcon icon={faLocationDot} /> Kigali City, Gasabo, Kimihurura</span>
-                <p>Published on 12 June | Expires on 12 July</p>
-                <p><b>Details:</b> 4 bedrooms, 2 bathrooms</p>
-                <div className="badges">
-                  <span className='type'>For Sale</span>
-                  <span className='price'><b>Price:</b> $200,000</span>
-                </div>
-                <button>View Property</button>
-              </Col>
-            </Stack>
-          </Card>
+          <HomeCard />
         </Col>
         <Col md={6}>
-          <Card>
-            <Stack direction="horizontal" gap={3}>
-              <Col className="image">
-                <img src={require('../../../assets/HouseOne 1.png')} alt="Card Image" />
-              </Col>
-              <Col className="content">
-                <h2>House for sale in Kigali</h2>
-                <span><FontAwesomeIcon icon={faLocationDot} /> Kigali City, Gasabo, Kimihurura</span>
-                <p>Published on 12 June | Expires on 12 July</p>
-                <p><b>Details:</b> 4 bedrooms, 2 bathrooms</p>
-                <div className="badges">
-                  <span className='type'>For Sale</span>
-                  <span className='price'><b>Price:</b> $200,000</span>
-                </div>
-                <button>View Property</button>
-              </Col>
-            </Stack>
-          </Card>
+          <HomeCard />
         </Col>
         <Col md={6}>
-          <Card>
-            <Stack direction="horizontal" gap={3}>
-              <Col className="image">
-                <img src={require('../../../assets/HouseOne 1.png')} alt="Card Image" />
-              </Col>
-              <Col className="content">
-                <h2>House for sale in Kigali</h2>
-                <span><FontAwesomeIcon icon={faLocationDot} /> Kigali City, Gasabo, Kimihurura</span>
-                <p>Published on 12 June | Expires on 12 July</p>
-                <p><b>Details:</b> 4 bedrooms, 2 bathrooms</p>
-                <div className="badges">
-                  <span className='type'>For Sale</span>
-                  <span className='price'><b>Price:</b> $200,000</span>
-                </div>
-                <button>View Property</button>
-              </Col>
-            </Stack>
-          </Card>
+          <HomeCard />
         </Col>
         <Col md={6}>
-          <Card>
-            <Stack direction="horizontal">
-              <Col className="image">
-                <img src={require('../../../assets/HouseOne 1.png')} alt="Card Image" />
-              </Col>
-              <Col className="content">
-                <h2>House for sale in Kigali</h2>
-                <span><FontAwesomeIcon icon={faLocationDot} /> Kigali City, Gasabo, Kimihurura</span>
-                <p>Published on 12 June | Expires on 12 July</p>
-                <p><b>Details:</b> 4 bedrooms, 2 bathrooms</p>
-                <div className="badges">
-                  <span className='type'>For Sale</span>
-                  <span className='price'><b>Price:</b> $200,000</span>
-                </div>
-                <button>View Property</button>
-              </Col>
-            </Stack>
-          </Card>
+          <HomeCard />
         </Col>
-        {/* <Col md={6}>
-          <Card className='row'>
-            <Col xs="1" sm="1" md="4" className='card-image'>
-              <img src={require('../../../assets/HouseOne 1.png')} alt='' />
-            </Col>
-            <Col xs="1" sm="1" md="8" className='card-details'>
-              <CardBody>
-                <h3>House for sale in Kigali</h3>
-                <span><FontAwesomeIcon icon={faLocationDot} /> Kigali City, Gasabo, Kimihurura</span>
-                <p>Published on 12 June | Expires on 12 July</p>
-                <p><b>Details:</b> 4 bedrooms, 2 bathrooms</p>
-                <div className="badges">
-                  <span className='type'>For Sale</span>
-                  <span className='price'><b>Price:</b> $200,000</span>
-                </div>
-                <button>View Property</button>
-              </CardBody>
-              
-            </Col>
-          </Card>
-        </Col> */}
       </div>
     </Row>
   )
