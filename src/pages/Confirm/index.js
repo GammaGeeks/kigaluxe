862296800db67ec5732e683532f41c2caaf41b8b
Igor J.L. <nigorjeanluc@gmail.com>
2024-08-01 21:41:02 +0200

    integrating login forms with Formik validations and redux

--- src/pages/Confirm/index.js
+++ src/pages/Confirm/index.js
@@ -1,9 +1,44 @@
 import React from 'react'
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col, Button, Form } from 'react-bootstrap'
 import {ReactComponent as Icon} from '../../assets/svg/envelope.svg'
 import FormInput from '../../components/Form/FormInput'
+import * as Yup from 'yup'
+import { useFormik } from 'formik'
+
+const ConfirmSchema = Yup.object().shape({
+  code: Yup.string().required('Required').min(6),
+})
+
 
 function Confirm() {
+  const {
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    errors,
+    values,
+  } = useFormik({
+    validationSchema: ConfirmSchema,
+    initialValues: {
+      code: '',
+    },
+    onSubmit: (values) => {
+        console.log(values)
+        const {
+            // eslint-disable-next-line no-unused-vars
+            code
+        } = values
+        // dispatch(userAction.signup({
+        //     firstname,
+        //     lastname,
+        //     email,
+        //     phone,
+        //     gender,
+        //     password
+        // })).then(() => navigate('/auth/confirm'));
+    }
+  });
+  
   return (
     <Container fluid>
       <Row className='signup-form'>
@@ -14,8 +49,17 @@ function Confirm() {
           <h1>Confirm your email</h1>
           <h3>Hi, firstname</h3>
           <p>Thank you for signing up with KigaluXe. Please confirm your email address <br />by filling the code sent to your email:</p>
-          <FormInput name="code" label="code" />
-          <Button variant='primary'>Confirm</Button>
+          <Form onSubmit={handleSubmit}>
+            <FormInput
+              name="code"
+              label="code"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.code}
+            />
+            {errors.code ? <p className="error-text text-center text-danger font-italic">{errors.code}</p> : ''}
+            <Button variant='primary'>Confirm</Button>
+          </Form>
         </Col>
       </Row>
     </Container>
